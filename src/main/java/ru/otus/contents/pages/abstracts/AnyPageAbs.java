9baf3ff94655ca7d677f28fe98b68223bd1689a6
Ruslan Badretdinov IBS Appline <RBadretdinov@appline.ru>
2024-04-12 11:39:31 +0300

    Удалил лишнюю переменную в AnyPageAbs, которую использовал в другом abstract классе наследнике AnyDynamicPageAbs. Удалил лишние методы, которые не использую

--- src/main/java/ru/otus/contents/pages/abstracts/AnyPageAbs.java
+++ src/main/java/ru/otus/contents/pages/abstracts/AnyPageAbs.java
@@ -3,18 +3,13 @@ package ru.otus.contents.pages.abstracts;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.apache.commons.lang3.StringUtils;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import ru.otus.annotations.UrlPrefix;
 import ru.otus.utils.fromdriver.CommonActions;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 
 
 public abstract class AnyPageAbs<T> extends CommonActions<T> {
-
-    private By dynamicPageValidationBy;
 
     public AnyPageAbs(WebDriver driver) {
         super(driver);
@@ -32,27 +27,6 @@ public abstract class AnyPageAbs<T> extends CommonActions<T> {
                 this.getClass().getSimpleName());
         assertThat(waiter.waitForElementVisible(elementViaFindBy)).as(pageIsNotLoadedText).isTrue();
         return (T) this;
-    }
-
-    public <T> T page(Class<T> clazz) {
-        try {
-            Constructor constructor = clazz.getConstructor(WebDriver.class);
-
-            return convertInstanceOfObject(constructor.newInstance(driver), clazz);
-
-        } catch (NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    private static <T> T convertInstanceOfObject(Object o, Class<T> clazz) {
-        try {
-            return clazz.cast(o);
-        } catch (ClassCastException e) {
-            return null;
-        }
     }
 
     protected String getBaseUrl() {
